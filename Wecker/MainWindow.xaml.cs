2b8a51468dac2a08a6b124bb88b78019e8035dbf
Robert Kirsten <Robert.Kirsten@swisslife.ch>
2018-08-07 07:53:46 +0100

    2. Teil

--- Wecker/MainWindow.xaml.cs
+++ Wecker/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,13 +15,46 @@ using System.Windows.Shapes;
 namespace Wecker
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Buttonadd_Click(object sender, RoutedEventArgs e)
+        {
+            string title = textboxtitle.Text;
+            string hoursText = textboxhours.Text;
+            string minutesText = textboxminutes.Text;
+            int hours;
+            int minutes;
+            bool result = int.TryParse(hoursText, out hours);
+            result = int.TryParse(minutesText, out minutes);
+
+            if(result && hours >= 0 && hours <=23 && minutes >=0 && minutes <=59)
+            {
+                Meeting m = new Meeting(title, hours, minutes);
+                listBoxMeetings.Items.Add(m);
+            }
+            else
+            {
+                //this is a message
+                MessageBox.Show("Fehler");
+            }
+       
+        }
+
+        private void Buttondel_Click(object sender, RoutedEventArgs e)
+        {
+            int i = listBoxMeetings.SelectedIndex;
+            if (i > 0)
+            {
+                listBoxMeetings.Items.RemoveAt(i);
+            }
+
         }
     }
 }
---
+++ Wecker/meeting.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wecker
+{
+    class Meeting
+    {
+        string title;
+        int h;
+        int m;
+
+        public Meeting(string title, int h, int m)
+        {
+            this.title = title;
+            this.h = h;
+            this.m = m;
+        }
+
+        public override string ToString()
+        {
+            string s = h.ToString("D2") + ":" + m.ToString("D2") + " - " + title;
+            return s;
+            
+        }
+
+        public virtual void Wake()
+        {
+            
+
+        }
+
+    }
+
+    class MeetingColor : Meeting
+    {
+        public MeetingColor(string title, int h, int m) : base(title, h, m)
+        {
+            
+        }
+
+        public override void Wake()
+        {
+            base.Wake();  //TODO FARBE ÄNDERN
+        }
+
+    }
+
+    class MeetingSound : Meeting
+    {
+        public MeetingSound(string title, int h, int m) : base(title, h, m)
+        {
+        }
+
+        public override void Wake()
+        {
+            System.Media.SystemSounds.Beep.Play();
+        }
+    }
+}
